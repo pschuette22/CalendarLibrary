81dfed9824193c1208721a9b2500840214dff5bd
Pete Schuette <pschuette22@gmail.com>
2015-02-11 17:34:09 -0500

    Release commit
    release version to be added to project

---
+++ gen/com/pschuette/android/calendarlibrary/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pschuette.android.calendarlibrary;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
